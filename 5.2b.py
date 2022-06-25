75e2114f2049c8fca2e421cc24deb672ad626331
itcatmeow <103352895+itcatmeow@users.noreply.github.com>
2022-06-25 12:13:57 +0800

    Lab 5

---
+++ 5.2b.py
@@ -0,0 +1,38 @@
+import socket
+import os
+from _thread import *
+
+ServerSocket = socket.socket()
+host=''
+port = 8889
+ThreadCount = 0
+try:
+        ServerSocket.bind((host,port))
+except socket.error as e:
+        print(str(e))
+
+print('Waiting for a Connection...')
+ServerSocket.listen(5)
+
+def threaded_client(connection):
+        connection.send(str.encode('Welcome to the Server\n'))
+        while True:
+                data = connection.recv(2048)
+                reply = 'Server Says:' + data.decode('utf-8')
+                if not data:
+                        break
+                connection.sendall(str.encode(reply))
+        connection.close()
+
+msg = ServerSocket.recv(1024)
+
+while True:
+        Client,address = ServerSocket.accept()
+        print('Connected to:' + address[0] + ':' + str(address[1]))
+        start_new_thread(threaded_client,(Client,))
+        ThreadCount +=1
+        print('Thread Number:' + str(ThreadCount))
+
+        print("Client says" + msg.decode())
+        msg = ServerSocket.recv(1024)
+ServerSocket.close()
